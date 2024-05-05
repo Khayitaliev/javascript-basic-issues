7d94a614a2e62e3eb34deda8b25c39b1bbf50e1e
Bekhruz <117586415+Khayitaliev@users.noreply.github.com>
2024-05-05 23:18:41 +0500

    update

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -3,18 +3,24 @@
 //Kiritish , chiqarish va o'zlashtirish operatoriga oid masalalar
 
 //Begin 1 . Kvadrat tomoni a berilgan . Uning perimetri aniqlansin . Formula : P = 4 * a;
-// let a = 20;
-// let p = a * 4;
+// let a = 20,
+//     p = a * 4;
 // console.log(`Kvadratning tomoni: ${a}\nUning perimetri: ${p}`)
 
 // Begin 2 . Kvadratning tomoni a berilgan. Uning yuzasi aniqlansin. S = a 2;
-// let a = 10;
-// let s = a ** 2;
+// let a = 10,
+//     s = a ** 2;
 // console.log(`Kvadratning tomoni:${a}\n Yuzasi:${s}`)
 
 // Begin 3 .To'g'ri to'rtburchakning tomonlari a va b berilgan . Uning yuzasi S = a * b ; va P = 2 * (a + b) perimetri aniqlansin
-let a = 15,
-    b = 22,
-    s = a * b,
-    p = 2 * (a + b);
-console.log(`Tomonlari : ${a} va ${b}\nYuzasi: ${s}\nPerimetri:${p}`)
+// let a = 15,
+//     b = 22,
+//     s = a * b,
+//     p = 2 * (a + b);
+// console.log(`Tomonlari : ${a} va ${b}\nYuzasi: ${s}\nPerimetri:${p}`)
+
+//Begin 4 . Aylananing diametri d berilgan.Uning uzunligi aniqlansin Formula L = p * d ; p = 3.14
+// const p = 3.14;
+// let d = 18,
+//     l = p * d;
+// console.log(`Diametri:${d}\nUzunligi:${l}`)
