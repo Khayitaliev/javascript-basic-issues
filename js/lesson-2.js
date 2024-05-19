ae3e8de4b277b3465e5bab0d282a07c65c82b76e
Bekhruz <117586415+Khayitaliev@users.noreply.github.com>
2024-05-19 11:11:00 +0500

    push

---
+++ js/lesson-2.js
@@ -0,0 +1,12 @@
+'use strict' //qat'iy rejim
+//Butun sonlarga oid masalalar
+
+//Integer 1 . Uzunlik L santimetrda berilgan. Undagi to'liq metrlar sonini aniqlovchi programma tuzilsin 1m = 100cm
+// let l = 10;
+// let parseMetre = l / 100;
+// console.log(`${l}sm ${parseMetre} metr ga teng`)
+
+//Integer 2 . Og'irlik M kilogramda berilgan. Undagi to'liq tonnalar sonini aniqlovchi programma tuzilsin. 1t = 1000kg
+let weight = 100234;
+let parseWeight = weight / 1000;
+console.log(`${weight}kg ${parseWeight} tonna ga teng`)
